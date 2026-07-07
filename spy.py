8f3cd867ade07bfee5b922e9b0eaaf7e12c7dad0
palshraddha26 <palshraddha52@gmail.com>
2026-07-07 20:40:54 +0530

    second

--- spy.py
+++ spy.py
@@ -4,3 +4,8 @@ print("hello")
 print("hello")
 print("hello")
 print("hello")
+# add two numbers
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+sum = a+b
+print("sum of two numbers is:",sum)
